a564e7c62d0d117c4f389c8cbd127883ea0bf3d7
mikibouns <mikibouns@gmail.com>
2019-06-07 14:30:15 +0200

    added registration view

--- mongo_project/auth_app/views.py
+++ mongo_project/auth_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponseRedirect
-from . forms import UserLoginForm
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from . forms import UserLoginForm, UserRegisterForm
 from django.contrib import auth
 
 
@@ -21,3 +21,30 @@ def login(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def registration(request):
+    # current_path = request.META.get('HTTP_REFERER')
+    if request.method == 'POST':
+        register_form = UserRegisterForm(request.POST, request.FILES)
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('main:main_page'))
+    else:
+        register_form = UserRegisterForm()
+    content = {'register_form': register_form}
+    return render(request, 'auth_app/register.html', content)
+
+
+# def edit(request):
+#     title = 'редактирование'
+#     if request.method == 'POST':
+#         edit_form = UserEditForm(request.POST, request.FILES,
+#                                      instance=request.user)
+#         if edit_form.is_valid():
+#             edit_form.save()
+#             return HttpResponseRedirect(reverse('auth:edit'))
+#     else:
+#         edit_form = UserEditForm(instance=request.user)
+#     content = {'title': title, 'edit_form': edit_form, 'collections': wallpaper_collections}
+#     return render(request, 'authapp/edit.html', content)
