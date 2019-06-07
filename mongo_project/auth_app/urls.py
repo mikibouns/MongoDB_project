dcfad4e7988a02b37d4432ae6cd24d0428c63cb0
mikibouns <mikibouns@gmail.com>
2019-06-07 14:30:49 +0200

    added registration view

--- mongo_project/auth_app/urls.py
+++ mongo_project/auth_app/urls.py
@@ -6,4 +6,5 @@ app_name = 'auth_app'
 urlpatterns = [
     path(r'login', views.login, name='login'),
     path(r'logout', views.logout, name='logout'),
+    path(r'registration', views.registration, name='reg')
 ]
